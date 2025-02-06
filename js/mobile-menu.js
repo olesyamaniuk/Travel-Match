59ed9a2842083a0d682842cefb929bb7385ee870
olesyamaniuk <maniukolesya222@gmail.com>
2025-02-06 14:46:33 +0200

    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -26,10 +26,10 @@ function toggleAnswer(element) {
 
   if (answer.style.display === "none" || answer.style.display === "") {
     answer.style.display = "block";
-    buttonImg.src = "/img/faq/arrow.png"; 
+    buttonImg.src = "/image/faq/navigation up (1).png"; 
   } else {
     answer.style.display = "none";
-    buttonImg.src = "/img/faq/icon.png"; 
+    buttonImg.src = "/image/faq/navigation up.png"; 
   }
 }
 
--- js/slick.js
+++ js/slick.js
@@ -19,6 +19,23 @@ $(function () {
 
 
 $(function () {
+  $('.features-list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.features-list-left',
+    nextArrow: '.features-list-right',
+  });
+});
+
+$(function () {
   $('.reviews-list').slick({
     infinite: true,
     slidesToShow: 1,
